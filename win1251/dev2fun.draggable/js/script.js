9cde297a13833a49eaf275a9e1656eb89b410a8f
darkfriend <maslov.tlt@gmail.com>
2025-06-01 14:15:43 +0300

    changed drag icon

--- win1251/dev2fun.draggable/js/script.js
+++ win1251/dev2fun.draggable/js/script.js
@@ -68,9 +68,7 @@ function globalInitDragAndDrop(propertyId, propertyType = 'input') {
                 const handle = document.createElement('span');
                 handle.className = 'drag-handle';
                 handle.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="12" height="12">
-                        <path d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z"/>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>
                 `;
                 row.querySelector('td').insertBefore(handle, row.querySelector('td').firstChild) //, row.firstChild);
             }
